7ece9f5e489ecfcd417830646d0c0ff04f51fba9
Nick Hernandez <nickhernandez@djlactose.com>
2019-03-12 13:04:58 -0400

    Changes to make the upgrade process smoother

--- adduser.sh
+++ adduser.sh
@@ -2,5 +2,5 @@
 read -p "Please enter the username: " user
 useradd -m $user
 ls /root/bastion/ |xargs -I xxx ln -s /root/bastion/xxx /home/$user/xxx
-BackupUsers.sh
+/root/bin/BackupUsers.sh
 sudo -u $user google-authenticator
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 /root/bin/upgrade.sh
-/usr/sbin/sshd -D
+/usr/sbin/sshd -f /etc/ssh/sshd_config -D
